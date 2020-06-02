1e8c339337b1b852446f3ecf81c6fec61183d735
Jose Ross Barredo <iamjoross@gmail.com>
2020-06-02 11:12:42 +0800

    added new string constants

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,4 @@ export const VIDEO = 'video';
 export const AUDIO = 'audio';
 export const TEXT = 'text';
 export const UPDATE_CURRENT_DROPPED_ITEM = 'updateCurrentDroppedItem';
-export const UPDATE_CURRENT_HOVERING_COORD = 'UPDATE_CURRENT_HOVERING_COORD';
+export const UPDATE_CURRENT_HOVERING_ITEM = 'updateCurrentHoveringItem';
